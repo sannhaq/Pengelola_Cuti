d867230644e5f2a50253faa6e3f76b5655315241
sannhaq <ikhsannhaq@gmail.com>
2024-01-09 15:54:19 +0700

    feat: config for connection

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // dependencies / libraries
+require('dotenv').config();
 const express = require('express');
+const { PrismaClient } = require('@prisma/client');
 const { configServer } = require('./src/configs/server.config');
 
 // routers
@@ -8,7 +10,15 @@ const { configServer } = require('./src/configs/server.config');
 const config = require('./src/configs/general.config');
 
 const port = config.port || 3000;
+const host = config.host || 'localhost';
 const app = express();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
 configServer(app);
 
 // endpoint
@@ -16,7 +26,11 @@ app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
+app.get('/test', (req, res) => {
+  res.send('Bintang baghdad anjay');
+});
+
 // logger
-app.listen(port, () => {
-  console.log(`listening at http://localhost:${port}`);
+const server = app.listen(port, host, () => {
+  console.log(`The server is running on http://${host}:${port}`);
 });
--- src/configs/general.config.js
+++ src/configs/general.config.js
@@ -3,4 +3,5 @@ require('dotenv').config();
 module.exports = {
   port: process.env.PORT,
   secret: process.env.SECRET_KEY,
+  host: process.env.DB_HOST,
 };
