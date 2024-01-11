5a6db6205fcd0fcfcbb8cbe6aec0aacedc038180
ranbojah <cp.zf.salman@gmail.com>
2024-01-11 15:32:30 +0700

    tes

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { loginValidation } = require('../validations/auth.validation');
-const { login, refresh, logout } = require('../controllers/auth.controller');
+const { login, refresh, logout, me } = require('../controllers/auth.controller');
 const auth = require('../middleware/auth.middleware');
 
 const router = express.Router();
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post('/login', loginValidation, login);
 router.post('/refresh', refresh);
 router.post('/logout', auth, logout);
+router.get('/me', auth, me);
 
 module.exports = router;
