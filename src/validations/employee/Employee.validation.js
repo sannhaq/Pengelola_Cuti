5d52a2b5e2d4c3846d6dffe1a25b77ae55fd8bfc
sannhaq <ikhsannhaq@gmail.com>
2024-01-24 12:15:23 +0700

    fix: employee validation

--- src/validations/employee/Employee.validation.js
+++ src/validations/employee/Employee.validation.js
@@ -2,25 +2,31 @@ const { z } = require('zod');
 const { validate } = require('../../utils/helper.util');
 
 const addEmployeeInputScheme = z.object({
-    body: z.object({
-        nik: z.string({
-            required_error: 'Nik Is Required',
-        }),
-        name: z.string({
-            required_error: 'Name Is Required',
-        }),
-        email: z.string({
-            required_error: 'Email Is Required',
-        }),
-    })
+  body: z.object({
+    nik: z
+      .string({
+        required_error: 'Nik Is Required',
+      })
+      .min(1),
+    name: z
+      .string({
+        required_error: 'Name Is Required',
+      })
+      .min(1),
+    email: z
+      .string({
+        required_error: 'Email Is Required',
+      })
+      .min(1),
+  }),
 });
 
 const editEmployeeInputScheme = z.object({
-    body: z.object({
-        name: z.string({
-            required_error: 'Name Is Required',
-        })
-    })
+  body: z.object({
+    name: z.string({
+      required_error: 'Name Is Required',
+    }),
+  }),
 });
 
 const addEmmployeeInputValidation = validate(addEmployeeInputScheme);
