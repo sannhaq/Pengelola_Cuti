0cb9e8767abf27fee06fa0e08087d5be215c74ea
ranbojah <cp.zf.salman@gmail.com>
2024-04-19 08:43:19 +0700

    fix: seeder

--- prisma/seeder/position.seeder.js
+++ prisma/seeder/position.seeder.js
@@ -1,19 +1,63 @@
 const { prisma } = require('./config');
 const { faker } = require('@faker-js/faker');
 
-const generateFakePositions = () => {
-  return {
-    name: faker.person.jobType(),
+const positions = [
+  {
+    name: 'Software Engineer',
     created_at: new Date(),
     updated_at: new Date(),
-  };
-};
+  },
+  {
+    name: 'Data Scientist',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    name: 'Product Manager',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    name: 'UI/UX Designer',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    name: 'Marketing Specialist',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    name: 'Financial Analyst',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    name: 'HR Manager',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    name: 'Sales Representative',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    name: 'Operations Manager',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    name: 'Customer Support Specialist',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+];
 
 async function positionSeed() {
-  for (let i = 0; i < 10; i++) {
-    const fakePosition = generateFakePositions();
+  for (let position of positions) {
     await prisma.positions.create({
-      data: fakePosition,
+      data: position,
     });
   }
 }
